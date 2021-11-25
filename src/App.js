afaae780fda3471bd031cb46710a5147214ea96a
Artem <arstrel@gmail.com>
2021-11-25 14:59:16 -0800

    Success build

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function Copyright() {
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://github.com/caitlinboldt">
-        Sincerely, Your Plantsky Caregiver
+        Sincerely, Plantsky Caregiver
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
